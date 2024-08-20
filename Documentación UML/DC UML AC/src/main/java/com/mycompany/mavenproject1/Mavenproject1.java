102e104076f5c9566a7e7234cf64f22a80a73ace
Marxcruz <crumarx140@gmail.com>
2024-08-20 11:09:02 -0500

    DC UML AC

---
+++ Documentación UML/DC UML AC/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author CPE_DS_APOYO
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
