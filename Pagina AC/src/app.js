daae7d477f282fc46d5769d43f7999a41fff0ff2
RodrigoPGM1 <haloperseus@gmail.com>
2024-09-13 10:54:01 -0500

    CHeckpoint

--- Pagina AC/src/app.js
+++ Pagina AC/src/app.js
@@ -2,30 +2,45 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const myconnection = require('express-myconnection');
 const mysql = require('mysql');
-const session= require('express-session');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.set('port',4000);
+app.set('port', 4000);
 
+// Configura el directorio de las vistas
 app.set('views', __dirname + '/views');
+
+// Configura Handlebars con el layout principal
 app.engine('.hbs', engine({
     extname: '.hbs',
+    layoutsDir: __dirname + '/views/Layout', // Ruta de la carpeta de layouts
 }));
+
 app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+app
 app.use(bodyParser.json());
 
+app.use(myconnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'sistemaac'
+}));
 
-
+// Inicia el servidor
 app.listen(app.get('port'), () => {
     console.log('Listening on port', app.get('port'));
 });
 
-app.get('/', (req,res) => {
-    res.render('home');
+
+// Ruta principal que renderiza la vista 'home'
+app.get('/', (req, res) => {
+    res.render('home'); 
 });
+
